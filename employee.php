b829f37114fe4f5ecd820e1f1ef7a789531867f8
Andrey Ovchinnikov <kukuruza-11@yandex.ru>
2017-04-25 10:58:07 +0300

    25.04.17

--- employee.php
+++ employee.php
@@ -7,18 +7,17 @@ require_once('App.php');
     <table border='1'>
 
         <?php
-$sqlin = "
+        $sql = "
     SELECT DISTINCT em.id AS em_id, CONCAT_WS(' ', em.firstname, em.lastname, em.patronymic) AS fio,
      em.gender, em.salary, (SELECT GROUP_CONCAT( dp.name ) AS deps
     FROM employee em
     JOIN employee_department emdp
       ON em.id = emdp.id_employee
     JOIN departments dp
-      ON emdp.id_department = dp.id  WHERE  em.id = em_id) AS ddd
+      ON emdp.id_department = dp.id  WHERE  em.id = em_id) AS departments
     FROM employee em";
 
-$sql = $app->request($sqlin); ?>
-
+        $result = $app->request($sql); ?>
 <tr>
 <td>ФИО</td>
 <td>Пол</td>
@@ -26,20 +25,20 @@ $sql = $app->request($sqlin); ?>
 <td>Отдел</td>
 <td></td>
 <td></td></tr>
-        <?php while ($res = mysqli_fetch_row($sql)) {
-            $arr = []; ?>
+
+        <?php while ($row = mysqli_fetch_assoc($result)) { ?>
     <tr>
-        <td><?= $res[1] ?></td>
-        <td><?= $res[2] ?></td>
-        <td><?= $res[3] ?></td>
-        <td width="300"><?= $res[4] ?></td>
-    <td><a href="/employee/update.php?id=<?=$res[0]?>">UPDATE</a>
-    <td><a href="/employee/delete.php?id=<?=$res[0]?>">DELETE</a>
+        <td><?= $row['fio'] ?></td>
+        <td><?= $row['gender'] ?></td>
+        <td><?= $row['salary'] ?></td>
+        <td width="300"><?= $row['departments'] ?></td>
+        <td><a href="/employee/create.php?id=<?= $row['em_id'] ?>&action=update">UPDATE</a>
+        <td><a href="/employee/create.php?id=<?= $row['em_id'] ?>&action=delete">DELETE</a>
     </tr>
 
 <?php } ?>
 
 </table>
-    <a class='action' href='./employee/create.php'>СОЗДАТЬ</a>
+    <a class='action' href='./employee/create.php?action=create'>СОЗДАТЬ</a>
 
 <?php require_once('./footer.php'); ?>
--- employee/create.php
+++ employee/create.php
@@ -1,59 +1,85 @@
 <?php
+
 require_once('../App.php');
+
+define('ACTION', $_GET['action']);
+
+
 if (isset($_POST['firstname'])) {
-    $sql = "INSERT INTO employee (firstname, lastname, patronymic, gender, salary)" .
-        " VALUES ('" . $_POST['firstname'] . "','"
-        . $_POST['lastname'] . "','"
-        . $_POST['patronymic'] . "','"
-        . $_POST['gender'] . "',"
-        . (integer)$_POST['salary'] . ")";
+    if ($_POST['action'] == 'create') {
+        $sql = "INSERT INTO employee (firstname, lastname, patronymic, gender, salary)" .
+            " VALUES ('" . $_POST['firstname'] . "','"
+            . $_POST['lastname'] . "','"
+            . $_POST['patronymic'] . "','"
+            . $_POST['gender'] . "',"
+            . (integer)$_POST['salary'] . ")";
 
-    $result = $app->request($sql);
+        $result = $app->request($sql);
 
-    if ($result) {
-        $sql = "SELECT id FROM employee ORDER BY id DESC LIMIT 1";
+        if ($result) {
+            $sql = "SELECT id FROM employee ORDER BY id DESC LIMIT 1";
 
-        $newemployee = $app->request($sql);
+            $newemployee = $app->request($sql);
 
-        if ($newemployee) {
-            $id = mysqli_fetch_assoc($newemployee);
+            if ($newemployee) {
+                $id = mysqli_fetch_assoc($newemployee);
 
-            if (isset($_POST['departments'])) {
-                foreach ($_POST['departments'] as $res) {
-                    $sql = "INSERT INTO employee_department
+                if (isset($_POST['departments'])) {
+                    foreach ($_POST['departments'] as $res) {
+                        $sql = "INSERT INTO employee_department
                                 VALUES (" . $id['id'] . ", " . (integer)$res . ")";
 
-                    $result = $app->request($sql);
-                    if ($result) {
-                        header("Location: http://" . $_SERVER['HTTP_HOST'] . "/employee.php");
-                    }
+                        $result = $app->request($sql);
+                        if ($result) {
+                            header("Location: http://" . $_SERVER['HTTP_HOST'] . "/employee.php");
+                        }
                     }
                 }
             }
         }
     }
+    if ($_POST['action'] == 'update') {
+        if (isset($_POST['firstname']) && isset($_POST['lastname'])) {
+            $sql = "DELETE  FROM employee_department WHERE id_employee =" . (integer)$_POST['id'];
+            $result = $app->request($sql);
 
-require_once('../header.php');
-require_once('../functions.php');
+            $sql = "UPDATE  employee" .
+                " SET  firstname = '" . $_POST['firstname'] . "', " .
+                "lastname = '" . $_POST['lastname'] . "', " .
+                "patronymic = '" . $_POST['patronymic'] . "', " .
+                "gender = '" . $_POST['gender'] . "', " .
+                "salary = " . $_POST['salary'] . " WHERE id=" . (integer)$_POST['id'];
+            $result = $app->request($sql);
 
-$sql = "SELECT id AS department_id, name AS department_name  FROM departments";
-
-// Создание сотрудника ...
-$employeeDepartments = [];
-$result = $app->request($sql);
-
-while ($row = mysqli_fetch_assoc($result)) {
-    if (!isset($formData)) {
-        if (!empty($row['firstname'])) {
-            $formData = $row;
+            if ($result) {
+                if (isset($_POST['departments'])) {
+                    foreach ($_POST['departments'] as $department) {
+                        $sql = "INSERT INTO employee_department VALUES (" .
+                            (integer)$_POST['id'] . ", " . (integer)$department . ")";
+                        $result = $app->request($sql);
+                        if ($result) {
+                            header("Location: http://" . $_SERVER['HTTP_HOST'] . "/employee.php");
+                        }
+                    }
+                } else {
+                    die("Не выбран отдел");
+                }
+            } else {
+                die("Данные не обновлены");
+            }
+        } else {
+            die('Не введено имя или фамилия');
         }
     }
-    if (!empty($row['firstname'])) {
-        array_push($employeeDepartments, $row['department_name']);
-    }
-    $allDepartments[$row['department_id']] = $row['department_name'];
 }
-// Вывод формы...
-include('./_form.php');
 
+if (isset($_GET['action']) && $_GET['action'] == 'delete') {
+    $sql = "DELETE  FROM employee WHERE id=" . (integer)$_GET['id'];
+    $result = $app->request($sql);
+    if ($result) {
+        header("Location: http://" . $_SERVER['HTTP_HOST'] . "/employee.php");
+    }
+}
+
+include('./_form.php');
 require_once('../footer.php');
